5aa61f1bc8b2dfcb74b302a34518959505f37a72
AbuTaher-vector <70890283+AbuTaher-vector@users.noreply.github.com>
2021-04-05 13:47:40 +0600

    Add files via upload

---
+++ uva913.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define nn int
+#define dd double
+#define ll long long int
+#define ss string
+int main()
+{      
+   
+   nn tt;
+   while(cin>>tt)
+   {
+   	ll x=7;
+   	ll k=tt/2;
+   	k--;
+   	ll add=10;
+   	while(k--)
+   	{
+   		x+=add;
+   		add+=4;
+
+   	}
+     cout<<x*3-6<<"\n";
+   }
+
+
+
+}
